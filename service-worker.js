6ea6a2892c44e44c13ad8ae502790c047d0561f2
omearajw <26517984@students.lincoln.ac.uk>
2024-10-20 13:10:11 -0400

    Trying to fix 404

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const urlsToCache = [
     '/index.html',
     '/styles.css',
     '/script.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/icons/icon192.png',
+    '/icons/icon512.png'
 ];
 
 // Install the service worker and cache resources
